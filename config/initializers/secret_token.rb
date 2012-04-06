773de126c2621c78963cad4955a82b1cb25f3c2f
Christopher Hein <me@christopherhein.com>
2012-04-06 13:43:15 -0700

    base setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrapePresentation::Application.config.secret_token = '3e0fc0ea178abf8cfdc01c873fb2d6d963233adc9ea69e1f5516b45c0cd43b9ff4f730b147f6451153fa57a4d261916c1f306aabc1200f73a40466150c781855'
